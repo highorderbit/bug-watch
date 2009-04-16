154f08d9c35ddde5bbd9b56571b7455f7a3ca510
John A. Debay <john@highorderbit.com>
2009-04-15 19:17:16 -0600

    Adding Xcode project template for a tab bar based application.

---
+++ src/Classes/FirstViewController.h
@@ -0,0 +1,16 @@
+//
+//  FirstViewController.h
+//  bug-watch
+//
+//  Created by John A. Debay on 4/15/09.
+//  Copyright High Order Bit, Inc. 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FirstViewController : UIViewController {
+
+}
+
+@end
---
+++ src/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  bug-watch
+//
+//  Created by John A. Debay on 4/15/09.
+//  Copyright High Order Bit, Inc. 2009. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
+
