9e761aa88ffd6bf7de711f166be7afd30f07eb1d
John A. Debay <john@highorderbit.com>
2009-05-12 17:12:08 -0600

    Adding ticket bin type.

---
+++ src/Classes/tickets/TicketBin.m
@@ -0,0 +1,58 @@
+//
+//  Copyright High Order Bit, Inc. 2009. All rights reserved.
+//
+
+#import "TicketBin.h"
+
+@interface TicketBin ()
+
+@property (nonatomic, copy) NSString * name;
+@property (nonatomic, copy) NSString * searchString;
+@property (nonatomic, assign) NSUInteger ticketCount;
+
+@end
+
+@implementation TicketBin
+
+@synthesize name, searchString, ticketCount;
+
++ (id)ticketBinWithName:(NSString *)aName searchString:(NSString *)aSearchString
+    ticketCount:(NSUInteger)aTicketCount
+{
+    return
+        [[[[self class] alloc] initWithName:aName searchString:aSearchString
+        ticketCount:aTicketCount] autorelease];
+}
+
+- (void)dealloc
+{
+    [name release];
+    [searchString release];
+    [super dealloc];
+}
+
+- (id)initWithName:(NSString *)aName searchString:(NSString *)aSearchString
+    ticketCount:(NSUInteger)aTicketCount
+{
+    if (self = [super init]) {
+        self.name = aName;
+        self.searchString = aSearchString;
+        self.ticketCount = aTicketCount;
+    }
+
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    return [self retain];
+}
+
+- (NSString *)description
+{
+    return
+        [NSString stringWithFormat:@"ticket bin: '%@', search string: '%@', "
+        "ticket count: %u", name, searchString, ticketCount];
+}
+
+@end
