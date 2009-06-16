16ad0b565faba0774cef736cd690282e67fa4681
Doug Kurth <doug@highorderbit.com>
2009-06-16 10:32:38 -0600

    Localized projects tab

--- src/Classes/projects/ProjectHomeViewController.m
+++ src/Classes/projects/ProjectHomeViewController.m
@@ -44,30 +44,38 @@
 
         cell = [nib objectAtIndex:0];
     }
-    
+
+    NSString * homeLabel = NSLocalizedString(@"projecthome.tabs.home", @"");
+    NSString * ticketsLabel =
+        NSLocalizedString(@"projecthome.tabs.tickets", @"");
+    NSString * milestonesLabel =
+        NSLocalizedString(@"projecthome.tabs.milestones", @"");
+    NSString * messagesLabel =
+        NSLocalizedString(@"projecthome.tabs.messages", @"");
+
     switch (indexPath.row)
     {
         case kProjectHome:
-            [cell setLabelText:@"Home"];
+            [cell setLabelText:homeLabel];
             [cell setImage:[UIImage imageNamed:@"HomeIcon.png"]];
             [cell
                 setHighlightedImage:
                 [UIImage imageNamed:@"HomeIconSelected.png"]];
             break;
         case kProjectTickets:
-            [cell setLabelText:@"Tickets"];
+            [cell setLabelText:ticketsLabel];
             [cell setImage:[UIImage imageNamed:@"Bug.png"]];
             [cell setHighlightedImage:[UIImage imageNamed:@"BugSelected.png"]];
             break;
         case kProjectMilestones:
-            [cell setLabelText:@"Milestones"];
+            [cell setLabelText:milestonesLabel];
             [cell setImage:[UIImage imageNamed:@"Calendar.png"]];
             [cell
                 setHighlightedImage:
                 [UIImage imageNamed:@"CalendarSelected.png"]];
             break;
         case kProjectMessages:
-            [cell setLabelText:@"Messages"];
+            [cell setLabelText:messagesLabel];
             [cell setImage:[UIImage imageNamed:@"Thumbtack.png"]];
             [cell
                 setHighlightedImage:
